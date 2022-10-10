e7a69be78d22a28c73a20724b3944f6d139dbf71
Julian <julianferrari3@gmail.com>
2022-10-10 10:38:27 -0300

    modificaciones

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
 import './App.css';
-import simpsonsApi from './APIrest/simpsonsApi';
-import Card from './Card'
-import './App.css'
-
-
+import Button from 'react-bootstrap/Button';
 
 function App() {  
   return ( //devuelve un JSX //Tengo que poner { } para que me lo tome como JS.
-    <div className="principal_container">
-      <h1>Characters</h1>
-      <div className='cards-container'>
-        {simpsonsApi.map((character, i) => (
-          <Card key={i}
-            name = {character.character}
-            image = {character.image}
-            quote = {character.quote}
-          />
-        ))}
-      </div>
+    <div>      
+      <Button variant="primary">Primary</Button>{" "}
     </div>
   );
 }
